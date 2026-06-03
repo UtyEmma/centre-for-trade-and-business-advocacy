bfe6fe762693de194883358147b6db326504bda4
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2026-06-03 23:12:59 +0100

    update the publications

--- app/Concerns/IsPublishable.php
+++ app/Concerns/IsPublishable.php
@@ -10,7 +10,9 @@ trait IsPublishable {
     public static function bootIsPublishable() {
         if(!request()->is('admin/*')) {
             static::addGlobalScope('published', fn($query) => $query->published());
-            if($this->hasAttribute('published_at')) {
+            $attr = new static;
+
+            if($attr->hasAttribute('published_at')) {
                 static::addGlobalScope('latestPublished', fn($query) => $query->latest('published_at'));
             }
         }
